f5f107868ef2e015b715b9d88e2c624c9b584bda
NDHDang <danggamingvnt@gmail.com>
2024-09-07 20:07:54 +0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+
+package student;
+
+
+public class Student {
+    int id;
+    String name;
+  
+    public static void main(String[] args) {
+ Student stu1 = new Student();
+        System.out.println(stu1.id + " " + stu1.name);
+    }
+    
+}
