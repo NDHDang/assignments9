527480fbc4f21017422ae149860925859c78cf08
NDHDang <danggamingvnt@gmail.com>
2024-09-07 20:21:26 +0700

    Add files via upload

---
+++ Caculation.java
@@ -0,0 +1,19 @@
+package caculation;
+
+public class Caculation {
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        Caculation obj = new Caculation();
+        System.out.println(obj.sum(20, 20));
+        System.out.println(obj.sum(10, 10, 10));
+    }
+
+}
